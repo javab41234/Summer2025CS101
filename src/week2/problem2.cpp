000dd74ec5d8a637e94d0069f0967dafa296b2c9
Javohir <j.bekdurdiyev@newu.uz>
2025-06-09 18:27:39 +0500

    nnf

--- src/week2/problem2.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    float x,y;
-    cin>>x>>y;
-    float t=3*(x*x+3)/(y/4+3);
-    cout<<"t="<<t<<endl;
-}
--- src/week2/problem5.cpp
+++
@@ -1,3 +0,0 @@
-//
-// Created by Javohir on 09.06.2025.
-//
