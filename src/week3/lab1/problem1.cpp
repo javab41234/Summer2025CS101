62b1f9675507fef541385b4d8525317c9b097409
Javohir <j.bekdurdiyev@newu.uz>
2025-06-16 18:50:24 +0500

    nnf21

---
+++ src/week3/lab1/problem1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+double prduct(double num1, double num2) {
+    return num1 * num2;
+}
+
+int main() {
+    double a, b;
+    cin>>a>>b;
+    cout<<prduct(a,b)<<endl;
+
+
+    return 0;
+}
