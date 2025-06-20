85de060fca9e4d74b3b3273ad7b43206591caaa5
Javohir <j.bekdurdiyev@newu.uz>
2025-06-20 17:51:51 +0500

    nnf21

--- src/week3/lab4/problem6.cpp
+++ src/week3/lab4/problem6.cpp
@@ -1,3 +1,24 @@
-//
-// Created by Javohir on 20.06.2025.
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    char arr[n];
+    int upper = 0, lower = 0, digit = 0, special = 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if (isupper(arr[i])) upper++;
+        else if (islower(arr[i])) lower++;
+        else if (isdigit(arr[i])) digit++;
+        else special++;
+    }
+
+    cout << "Uppercase:" << upper << endl;
+    cout << "Lowercase:" << lower << endl;
+    cout << "Digit:" << digit << endl;
+    cout << "Special:" << special << endl;
+
+    return 0;
+}
