55eeaacf0f978ad49b0c0281bbda5646c25e9b60
Javohir <j.bekdurdiyev@newu.uz>
2025-06-26 15:03:55 +0500

    nnf21

--- src/week4/lab2/problem1.cpp
+++ src/week4/lab2/problem1.cpp
@@ -1,3 +1,13 @@
-//
-// Created by Javohir on 24.06.2025.
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10] = {10, 23, 5, 67, 89, 33, 21, 78, 42, 9};
+    int *ptrA = arr;
+
+    for (int i = 0; i < 10; i++) {
+        cout << "Address: " << (ptrA + i) << " Value: " << *(ptrA + i) << endl;
+    }
+
+    return 0;
+}
--- src/week4/lab2/problem3.cpp
+++ src/week4/lab2/problem3.cpp
@@ -1,3 +1,24 @@
-//
-// Created by Javohir on 24.06.2025.
-//
+#include <iostream>
+using namespace std;
+
+int evenCount(const int* a, int size) {
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        if (*(a + i) % 2 == 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int size;
+    cin >> size;
+    int arr[size];
+
+    for (int i = 0; i < size; i++)
+        cin >> arr[i];
+
+    cout << evenCount(arr, size);
+    return 0;
+}
